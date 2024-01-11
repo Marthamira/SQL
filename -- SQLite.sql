4d6395c36030190e963b3161ae19cf00bb7c9194
Martha <atuhairemartha62@gmail.com>
2024-01-11 10:36:22 +0300

    Sql

---
+++ -- SQLite.sql
@@ -0,0 +1,9 @@
+-- SQLite
+-- CREATE TABLE users (
+--     username TEXT NOT NULL PRIMARY  KEY,
+--     password TEXT NOT NULL,
+--     email TEXT NOT NULL
+-- );
+
+INSERT INTO users (username, password, email)
+VALUES ('Martha', '1234' ,'martha@gmail.com');
